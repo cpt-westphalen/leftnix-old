3dd3d1d6b4ef88efe17d19e185d599118c877f76
cpt-westphalen <rbwestphalen@gmail.com>
2022-08-28 00:36:02 -0300

    new: ModalDesc component

--- src/components/ModalDesc/index.jsx
+++ src/components/ModalDesc/index.jsx
@@ -1,16 +1,32 @@
-export default function ModalDesc(movie) {
+import { useState } from 'react';
+import './styles.css';
+
+export default function ModalDesc({ movie }) {
+	const [show, setShow] = useState(false);
+
+	function handleShow() {
+		setShow(!show);
+	}
 	return (
 		<div className="modal-desc">
 			<img
 				className="modal-desc-cover"
-				width="280px"
+				height="250px"
 				alt="movie poster"
 				src={movie.imgPath || '#'}
 			/>
-			<p className="modal-desc-text">
-				{movie.desc ||
-					'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum numquam quod exercitationem quibusdam in dicta.'}
-			</p>
+
+			<div>
+				<p className={`modal-desc-text ${show ? 'show' : 'line-clamp'}`}>
+					{movie.desc}
+				</p>
+			</div>
+
+			{!show && (
+				<div className="modal-desc-show" onClick={handleShow}>
+					show more
+				</div>
+			)}
 			<div className="modal-desc-social">social area</div>
 		</div>
 	);
--- src/components/VideoModal/index.jsx
+++ src/components/VideoModal/index.jsx
@@ -3,7 +3,21 @@ import Button from '../Button';
 import MovieRating from '../MovieRating';
 import ModalDesc from '../ModalDesc';
 
-export function VideoModal({ movie, setIsModalOpen }) {
+const mockMovie = {
+	title: 'Default Movie Title',
+	imgPath:
+		'https://upload.wikimedia.org/wikipedia/en/7/78/Stranger_Things_season_4.jpg',
+	rating: 5,
+	desc: `Est ea nihil explicabo. Nihil ea omnis libero assumenda fuga similique veritatis. Ut laboriosam sit eum. Blanditiis dolor pariatur sunt aut numquam quasi. Iure repudiandae quos est ea nisi.
+
+Nisi necessitatibus odit ullam numquam. Sint dignissimos cumque voluptate laudantium porro minus reiciendis. Hic dolor voluptatem saepe doloribus voluptates sequi repellat.
+
+Nisi necessitatibus odit ullam numquam. Sint dignissimos cumque voluptate laudantium porro minus reiciendis. Hic dolor voluptatem saepe doloribus voluptates sequi repellat.
+
+Rerum sapiente commodi et. Et inventore sint est rerum qui odio recusandae. Omnis voluptas dolorum distinctio.`,
+};
+
+export function VideoModal({ movie = mockMovie, setIsModalOpen }) {
 	const handleClickModal = (e) => {
 		setIsModalOpen(false);
 	};
@@ -43,10 +57,3 @@ export function VideoModal({ movie, setIsModalOpen }) {
 		</div>
 	);
 }
-VideoModal.defaultProps = {
-	movie: {
-		title: 'Default Movie Title',
-		imgPath: '#',
-		rating: 5,
-	},
-};
